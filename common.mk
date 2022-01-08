2f0ae2d35c6e0a36e6d41ac8cd6fe6e9e5b1bbe8
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2022-01-08 11:30:41 +0900

    Update bluetooth audio HIDL impl to 2.1
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f

--- common.mk
+++ common.mk
@@ -125,12 +125,12 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     bt_configstore.conf \
     com.qualcomm.qti.bluetooth_audio@1.0 \
     libbtconfigstore \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
