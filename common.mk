6ec78ef59ebae1a154014e092b5e686ec473c632
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2022-01-08 16:20:19 +0900

    build needed camera packages
    Change-Id: I4cee2d83b929f3c3cbe417a5e5c4eb088247c152

--- common.mk
+++ common.mk
@@ -149,11 +149,13 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.device@3.5 \
     android.hardware.camera.device@3.6 \
+    android.hardware.camera.device@3.6.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-legacy \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.5-legacy \
     android.hardware.camera.provider@2.6 \
+    android.hardware.camera.provider@2.6.vendor \
     libcamera2ndk_vendor \
     libgui_vendor \
     vendor.qti.hardware.camera.device@1.0 \
