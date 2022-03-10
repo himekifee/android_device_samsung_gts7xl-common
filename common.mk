98d7555a2e6b50bde84bc5d8e6764289948e70e2
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2022-03-10 23:41:45 +0900

    Move libnfc-nci.conf to /product
    Device-specific files belong off /system.
    
    Bug: 117878616
    Test: make
    
    [fcuzzocrea]: drop also extract script patch..
    
    Change-Id: Iddad62c178c553e4d628deb4509ea54451b8ab4f

--- common.mk
+++ common.mk
@@ -433,7 +433,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 # OMX
 PRODUCT_PACKAGES += \
