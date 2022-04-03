ae412076322f40dec0bf2f1e5054a7f947495100
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2022-04-03 10:01:02 +0900

    Override vbmeta version in kernel cmdline
    * Somehow Samsung bootloader stops reporting everything about
      vbmeta after flashing a disabled image, so we need to set
      the version of it here to ensure runtime vs. compatibility
      matrix check won't fail on OTA.
    
    Change-Id: Ia26a84cb38e2f03025809a3ec52587481a6d8217

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,6 +94,7 @@ DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.1
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
